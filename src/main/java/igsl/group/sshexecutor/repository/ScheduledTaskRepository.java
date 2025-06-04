5c82b79745893a68365f746d8dc6004299529e4e
kenny.siu <iskenny4@gmail.com>
2025-06-04 17:12:26 +0800

    implement scheduler feature

---
+++ src/main/java/igsl/group/sshexecutor/repository/ScheduledTaskRepository.java
@@ -0,0 +1,15 @@
+package igsl.group.sshexecutor.repository;
+
+import igsl.group.sshexecutor.entity.ScheduledTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ScheduledTaskRepository extends JpaRepository<ScheduledTask, Long> {
+    List<ScheduledTask> findByActive(boolean active);
+    List<ScheduledTask> findBySshConfigId(Long sshConfigId);
+    boolean existsByNameAndIdNot(String name, Long id);
+    boolean existsByName(String name);
+}
